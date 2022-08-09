d906028eff54d7d70a88292b7b2c21b009e7401c
Terbeche Mustapha <termus96@gmail.com>
2022-08-09 17:07:42 +0100

    Update and delete from the animals table and take screenshots of the results of the queries

--- queries.sql
+++ queries.sql
@@ -8,3 +8,47 @@ SELECT name,escape_attempts from animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+BEGIN;
+
+UPDATE animals
+SET species = 'unspecified';
+
+ROLLBACK;
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name like '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species is null;
+
+COMMIT;
+
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+
+BEGIN;
+
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT sp1;
+
+UPDATE animals
+SET weight_kg = -weight_kg;
+
+ROLLBACK TO SAVEPOINT sp1;
+
+UPDATE animals
+SET weight_kg = -weight_kg
+WHERE weight_kg < 0;
+
+COMMIT;
