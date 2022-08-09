fce687f0dd487baabd84ca0ec8fc598138e51b8a
Terbeche Mustapha <termus96@gmail.com>
2022-08-09 17:42:05 +0100

    Add queries to queries.sql and screen shots of the results in the screen folder

--- queries.sql
+++ queries.sql
@@ -9,7 +9,7 @@ SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
-
+-- Update the animals table by setting the species column to unspecified then Rollback.
 BEGIN;
 
 UPDATE animals
@@ -17,38 +17,68 @@ SET species = 'unspecified';
 
 ROLLBACK;
 
+-- Start transaction
 BEGIN;
 
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
 UPDATE animals
 SET species = 'digimon'
 WHERE name like '%mon';
 
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
 UPDATE animals
 SET species = 'pokemon'
 WHERE species is null;
 
+-- Commit the transaction.
 COMMIT;
 
-
+-- Delete all records in the animals table, then roll back the transaction.
 BEGIN;
 DELETE FROM animals;
 ROLLBACK;
 
-
+-- Start transaction
 BEGIN;
 
+-- Delete all animals born after Jan 1st, 2022.
 DELETE FROM animals
 WHERE date_of_birth > '2022-01-01';
 
+-- Create a savepoint for the transaction.
 SAVEPOINT sp1;
 
+-- Update all animals' weight to be their weight multiplied by -1.
 UPDATE animals
 SET weight_kg = -weight_kg;
 
+-- Rollback to the savepoint
 ROLLBACK TO SAVEPOINT sp1;
 
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
 UPDATE animals
 SET weight_kg = -weight_kg
 WHERE weight_kg < 0;
 
+-- Commit transaction
 COMMIT;
+
+
+-- How many animals are there?
+SELECT  COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT  COUNT(*) AS "Animals who never escaped" FROM animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT  AVG(weight_kg) AS "Average weight" FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT  neutered,SUM(escape_attempts) FROM animals GROUP BY neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT  species,MIN(weight_kg) AS minimum weight, MAX(weight_kg) AS maximum weight FROM animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT  species,ROUND(AVG(escape_attempts)) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
