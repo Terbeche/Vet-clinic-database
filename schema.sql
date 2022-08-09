a856077cde7c5c631cef4f2798ca27feb4bc18c1
Terbeche Mustapha <termus96@gmail.com>
2022-08-09 22:15:35 +0100

    Create owners and species tables

--- schema.sql
+++ schema.sql
@@ -15,3 +15,16 @@ CREATE TABLE animals (
 ALTER TABLE animals
   ADD species VARCHAR(500);
 
+
+CREATE TABLE owners (
+    id            INT GENERATED ALWAYS AS IDENTITY,
+    full_name     VARCHAR(100),
+    age           INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+    id            INT GENERATED ALWAYS AS IDENTITY,
+    name          VARCHAR(100),
+    PRIMARY KEY(id)
+);
