566a3c9a75cf1edaa4f0879d5e46e19a680168e6
Terbeche Mustapha <termus96@gmail.com>
2022-08-10 14:55:14 +0100

    Create vets,specializations, and visits tables and insert data into them

--- schema.sql
+++ schema.sql
@@ -52,3 +52,33 @@ ALTER TABLE animals
 ADD CONSTRAINT fk_animals_owners
 FOREIGN KEY (owner_id)
 REFERENCES owners(id);
+
+
+-- Create a table named vets
+CREATE TABLE vets (
+    id            INT GENERATED ALWAYS AS IDENTITY,
+    name          VARCHAR(100),
+    age           INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+-- Create a join table named specializations
+CREATE TABLE specializations (
+    vets_id        INT,
+    species_id     INT, 
+    PRIMARY KEY(vets_id, species_id),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id),
+    CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id)
+);
+
+
+-- Create a join table named visits
+CREATE TABLE visits (
+    animals_id    INT,
+    vets_id       INT,
+    date_of_visit DATE, 
+    PRIMARY KEY(animals_id, vets_id, date_of_visit),
+    CONSTRAINT fk_animals FOREIGN KEY(animals_id) REFERENCES animals(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
